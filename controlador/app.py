383e8073af47016b154e826e1a497cf7934f536a
JChino33 <mpalato405@accitesz.com>
2021-10-25 19:54:36 -0500

    usuarios y carpetas
    creacion de las carpetas sugerencias y productos

--- controlador/app.py
+++ controlador/app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request
 
-app=Flask(__name__, template_folder='../vista')
+app=Flask(__name__, template_folder='../vista', static_folder='../static')
 
 @app.route('/')
 def inicio():
@@ -20,6 +20,14 @@ def registrarUsuario():
 @app.route('/usuarios/editarUsuario')
 def editarUsuario():
     return render_template('usuarios/editar.html')
+
+@app.route('/usuarios/login')
+def login():
+    return render_template('usuarios/login.html')
+
+@app.route('/usuarios/consultarUsuarios')
+def consultarUsuarios():
+    return render_template('usuarios/consultar.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
